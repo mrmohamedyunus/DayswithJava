bba7ab71cd45ae96bd9652c3b050b6f8e0a1d488
Mohamed Yunus <yunustaj222@gmail.com>
2026-06-16 08:59:32 +0530

    Add files via upload
    Scenario : The LMS Login Bug
    Context: You built a Learning Management System (LMS) that sends students a temporary One-Time Password (OTP) as a String, like this: String systemOtp = "0042";.
    However, a student types their input into a numeric keypad, which saves their answer as an integer: int studentInput = 42;.
    
    Because one is a String and one is an int, you cannot simply use == to compare them.
    
    Your Task: Write an if statement that successfully checks if the student's integer input mathematically matches the system's String OTP.

---
+++ lms.java
@@ -0,0 +1,10 @@
+public class lms{
+    public static void main(String[] args) {
+        String systemOtp = "0042";
+int studentInput = 42;
+
+if (Integer.parseInt(systemOtp) == studentInput) {
+    System.out.println("OTP Matched");
+}
+    }
+}
