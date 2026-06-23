c92f26853d689defe34533cd5b9f30995f86e8a9
Mohamed Yunus <yunustaj222@gmail.com>
2026-06-23 09:09:03 +0530

    Delete flight.java

--- flight.java
+++
@@ -1,5 +0,0 @@
-public class flight{
-    public static void main(String[] args);
-    String beacon = "  nav-beacon: MANILA-2026  ";
-    String cleanCity = beacon.trim().split(":")[1].split("-")[0].toLowerCase();
-}
